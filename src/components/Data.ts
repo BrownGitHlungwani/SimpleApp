62abf870abd37824af25754ce40f1456776b3840
BrownGitHlungwani <blesson.nkateko@gmail.com>
2024-02-13 13:45:47 +0200

    feat: edited Data

---
+++ src/components/Data.ts
@@ -0,0 +1,153 @@
+export const Assessments = [
+    {
+      id: 1,
+      name: "University's Mission, Values, and Culture",
+      task: "Task: Watch video presentations and read articles about the university's mission, values, and culture",
+      gamification: "Gamification: Earn points and badges for completing the modules and quizzes related to university culture",
+      imageSrc: '/leadership.png',
+      heading: "Orientation and Welcome Session"
+    },
+    {
+      id: 2,
+      name: "Campus Tour and Familiarization",
+      task: "Task: Take a virtual campus tour with 360-degree photos and interactive maps",
+      gamification: "Gamification: Complete challenges by visiting key locations virtually and earn points for exploration",
+      imageSrc: '/campus.png',
+      heading: "Orientation and Welcome Session"
+    },
+    {
+      id: 3,
+      name: "Completion of Employment Contracts and Forms",
+      task: "Task: Fill out employment contracts, tax forms, and legal documents online",
+      gamification: "Gamification: Earn points for each form completed accurately and within deadlines",
+      imageSrc: '/document.png',
+      heading: "HR Paperwork and Documentation"
+    },
+    {
+      id: 4,
+      name: "Submission of Identification and Qualification Documents",
+      task: "Task: Upload scanned copies of identification documents (e.g., ID, passport) and academic qualifications",
+      gamification: "Gamification: Receive badges for document submission and verification",
+      imageSrc: 'graduate.png',
+      heading: "HR Paperwork and Documentation"
+    },
+    {
+      id: 5,
+      name: "University Email Account Setup",
+      task: "Task: Set up a university email account with step-by-step instructions",
+      gamification: "Gamification: Earn points for successfully configuring the email account on personal devices",
+      imageSrc: '/sign.png',
+      heading: "IT Setup"
+    },
+    {
+      id: 6,
+      name: "Assistance with Device Setup",
+      task: "Task: Access video tutorials and guides for configuring personal devices for work purposes",
+      gamification: "Gamification: Points awarded for completing device setup tasks",
+      imageSrc: 'https://i.pinimg.com/564x/33/e8/07/33e807c32153e8f8712edb34ea990890.jpg',
+      heading: "IT Setup"
+    },
+    {
+      id: 7,
+      name: "Access Cards and Keys",
+      task: "Task: Request access cards or keys online, with approval processes integrated",
+      gamification: "Gamification: Achieve a Facility Access Pro badge for completing access-related tasks",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Access to Systems and Facilities"
+    },
+    {
+      id: 8,
+      name: "Academic and Administrative Systems",
+      task: "Task: Gain access to academic and administrative systems through a single sign-on portal",
+      gamification: "Gamification: Earn points for accessing and navigating these systems efficiently",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Access to Systems and Facilities"
+    },
+  
+    {
+      id: 9,
+      name: "Mandatory Training Enrollment",
+      task: "Task: Enroll in mandatory training programs and compliance courses",
+      gamification: "Gamification: Earn points for completing each training module, with certificates as rewards",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Training and Professional Development"
+    },
+    {
+      id: 10,
+      name: "Professional Development Opportunities",
+      task: "Task: Browse and register for available workshops, conferences, and skill-building sessions",
+      gamification: "Gamification: Gain access to exclusive skill-building challenges with points and skill badges ",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Training and Professional Development"
+    },
+    {
+      id: 11,
+      name: "Introduction to Department and Colleagues",
+      task: "Task: View department-specific introductory videos and profiles of colleagues.",
+      gamification: "Gamification: Connect with colleagues through virtual meet-and-greet sessions and earn social badges",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Academic Department Onboarding"
+    },
+  
+  
+    {
+      id: 12,
+      name: "Department Policies and Procedures",
+      task: "Task: Access and acknowledge department-specific policies and procedures",
+      gamification: "Gamification: Complete policy quizzes for points and compliance badges",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Academic Department Onboarding"
+    },
+    {
+      id: 13,
+      name: "Library Orientation",
+      task: "Task: Attend virtual library orientation sessions or explore an interactive library tour",
+      gamification: "Gamification: Participate in library treasure hunts and earn library explorer badges.",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Library Access and Research Resources"
+    },
+    {
+      id: 14,
+      name: "Access to Research Databases",
+      task: "Task: Gain access to research databases and academic journals with a single login",
+      gamification: "Gamification: Complete database searches and earn researcher badges",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Library Access and Research Resources"
+    },
+  
+  
+    {
+      id: 15,
+      name: "Benefits Explanation",
+      task: "Task: View multimedia presentations explaining employee benefits",
+      gamification: "Gamification: Complete benefits quizzes and earn benefits knowledge badges.",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "HR Benefits and Policies"
+    },
+    {
+      id: 16,
+      name: "Policy Review",
+      task: "Task: Review and acknowledge university policies on work hours, leave, and HR matters",
+      gamification: "Gamification: Earn compliance badges for policy acknowledgment",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "HR Benefits and Policies"
+    },
+    {
+      id: 17,
+      name: "Campus Services Information",
+      task: "Task: Access information on campus dining, transportation, wellness services, and parking",
+      gamification: "Gamification: Use virtual coupons for dining, earn transit points, or unlock wellness challenges",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Campus Services"
+    },
+    {
+      id: 18,
+      name: "Parking Permits and Transportation Cards",
+      task: "Task: Apply for parking permits or public transportation cards online",
+      gamification: "Gamification: Receive parking hero badges for successfully securing parking permits",
+      imageSrc: 'https://i.pinimg.com/564x/e3/a6/ea/e3a6eac58326cd4f32118f57fb08bd16.jpg',
+      heading: "Campus Services"
+    },
+  
+]
+  
